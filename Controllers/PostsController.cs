a5daf4456f0948ba3331bd4647a47bec917053c4
nddarnell_Desktop <darnell.nathaniel95@gmail.com>
2022-07-01 22:39:46 -0700

    Updated edit function
    Updated edit function for post controller

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -88,8 +88,7 @@ namespace BlogProject.Controllers
             {
                 return NotFound();
             }
-            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Description", post.BlogId);
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", post.BlogUserId);
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Name", post.BlogId);
             return View(post);
         }
 
@@ -98,7 +97,7 @@ namespace BlogProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,BlogUserId,Title,Abstract,Content,Created,Updated,ReadyStatus,Slug,ImageData,ContentType")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus,Image")] Post post)
         {
             if (id != post.Id)
             {
@@ -109,6 +108,7 @@ namespace BlogProject.Controllers
             {
                 try
                 {
+                    post.Updated = DateTime.UtcNow;                
                     _context.Update(post);
                     await _context.SaveChangesAsync();
                 }
@@ -126,6 +126,7 @@ namespace BlogProject.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Description", post.BlogId);
+            // Could possibly remove this
             ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", post.BlogUserId);
             return View(post);
         }
