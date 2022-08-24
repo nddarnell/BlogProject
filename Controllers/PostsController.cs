caea39c0f424ac504efb057efe5e0bbded033d87
nddarnell_Desktop <darnell.nathaniel95@gmail.com>
2022-08-23 22:25:02 -0700

    Updated post tags javascript

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -45,6 +45,7 @@ namespace BlogProject.Controllers
             var post = await _context.Posts
                 .Include(p => p.Blog)
                 .Include(p => p.BlogUser)
+                .Include(p => p.Tags)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (post == null)
             {
